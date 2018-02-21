c4e90a7da9cf6971557e2e071bef7588f67b4c0b
Adam Góra <adamgora@icloud.com>
2018-02-21 18:30:59 +0100

    Wstepna zabawa z dzwiekiem w HTML5

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+import sound1 from './sounds/simonSound1.mp3';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    playSound() {
+        console.log(this.audioContainer);
+        this.audioContainer.load();
+        this.audioContainer.play();
+    }
+    render() {
+        return (
+            <div className="App">
+                <button onClick={() => this.playSound()}>Click sound</button>
+                <audio id="audio" ref={(audio) => { this.audioContainer = audio; }}>
+                    <source id="src_mp3" type="audio/mp3" src={sound1}/>
+                </audio>
+            </div>
+        );
+    }
 }
 
 export default App;
