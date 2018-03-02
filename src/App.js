6d9af5bcecc7fc4939ca88be5c759b3cea107786
Adam Góra <adamgora@icloud.com>
2018-03-02 19:15:33 +0100

    Rozpoczynanie gry, odgrywanie pierwszego dźwieku, animacja przycisków

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import sound3 from './sounds/simonSound3.mp3';
 import sound4 from './sounds/simonSound4.mp3';
 
 class App extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            gameQueue: [1, 2, 3, 4, 1, 2, 3, 4],
+            gameQueue: [],
             level: 0,
             intervalID: null,
             currentQueueItem: 0,
+            activeButton: null,
             sounds: {
                 1: sound1,
                 2: sound2,
@@ -21,7 +22,8 @@ class App extends Component {
                 4: sound4
             },
             soundPlaying: null,
-            power: 0
+            power: 0,
+            started: 0
         };
     }
 
@@ -38,12 +40,58 @@ class App extends Component {
      */
     toggleGamePower() {
         this.setState({
-            power: !this.state.power
+            power: !this.state.power,
+            started: 0
         });
     }
 
+    /**
+     * Start new game
+     */
+
+    startGame() {
+        if (!this.state.power) {
+            return false;
+        }
+        this.resetGame();
+        this.move();
+        this.setState({
+            started: 1,
+        });
+
+    }
+
+    /**
+     * Reset game state
+     */
+    resetGame() {
+        this.setState({
+            gameQueue: [],
+            level: 0,
+        })
+    }
+
+    /**
+     * Game move
+     * Add sound to array and play all sounds queued
+     */
+    move() {
+        this.drawNext();
+        this.playQueue();
+    }
+
+    /**
+     * Draws next element and adds it to game queue
+     */
+    drawNext() {
+        const next = Math.floor(Math.random() * 4) + 1;
+        const gameQueue = [...this.state.gameQueue];
+        gameQueue.push(next);
+        this.setState({gameQueue});
+    }
+
     setInterval() {
-        const intervalID = setInterval(this.playItem.bind(this), 1000);
+        const intervalID = setInterval(this.playItem.bind(this), 500);
         this.setState({intervalID});
     }
 
@@ -51,69 +99,69 @@ class App extends Component {
         clearInterval(this.state.intervalID);
     }
 
-    pushNextToQueue(next) {
-        const gameQueue = [...this.state.gameQueue];
-        gameQueue.push(next);
-        this.setState({gameQueue});
-    }
-
-    startGame() {
-        this.drawNext();
-    }
-
     playQueue() {
-        this.setState({ currentQueueItem: 0 });
+        this.setState({currentQueueItem: 0});
         this.setInterval();
     }
 
     playItem() {
         this.playSound(this.state.currentQueueItem);
-        this.setState({ currentQueueItem: this.state.currentQueueItem + 1 });
-        if(this.state.currentQueueItem > 5) {
+        this.highlightButton(this.state.currentQueueItem);
+        this.setState({currentQueueItem: this.state.currentQueueItem + 1});
+        if (this.state.currentQueueItem > this.state.gameQueue.length) {
             this.unsetInterval();
+            this.unhiglightButtons(null);
         }
-    }
-
-    drawNext() {
-        const next = this.random();
-        this.pushNextToQueue(next);
-    }
-
-    random() {
-        return Math.floor(Math.random() * 4) + 1;
     }
 
     playSound(index) {
         const soundIndex = this.state.gameQueue[index];
-        this.setState({ soundPlaying: this.state.sounds[soundIndex] },function(){
+        this.setState({soundPlaying: this.state.sounds[soundIndex]}, function () {
             this.audioContainer.pause();
             this.audioContainer.load();
             this.audioContainer.play();
         })
     }
 
+    highlightButton(index) {
+        this.setState({
+            activeButton: this.state.gameQueue[index]
+        })
+    }
+
+    unhiglightButtons() {
+        this.setState({
+            activeButton: null
+        })
+    }
+
     render() {
         return (
             <div className="App">
-                <button onClick={()=>this.playQueue()}>Start test</button>
-                <audio id="audio" ref={(audio) => { this.audioContainer = audio; }}>
+                <button onClick={() => this.playQueue()}>Start test</button>
+                <audio id="audio" ref={(audio) => {
+                    this.audioContainer = audio;
+                }}>
                     <source id="src_mp3" type="audio/mp3" src={this.state.soundPlaying}/>
                 </audio>
                 <div id="game-container">
-                    <div className="game-button btn-lt btn-green"></div>
-                    <div className="game-button btn-rt btn-red"></div>
-                    <div className="game-button btn-lb btn-yellow"></div>
-                    <div className="game-button btn-rb btn-blue"></div>
+                    <div className={(this.state.activeButton == 1 ? 'active' : '') + " game-button btn-lt btn-green"}></div>
+                    <div className={(this.state.activeButton == 2 ? 'active' : '') + " game-button btn-rt btn-red"}></div>
+                    <div className={(this.state.activeButton == 3 ? 'active' : '') + " game-button btn-lb btn-yellow"}></div>
+                    <div className={(this.state.activeButton == 4 ? 'active' : '') + " game-button btn-rb btn-blue"}></div>
                     <div id="simon-center">
                         <div id="game-title">simon</div>
                         <div id="game-controls">
                             <div id="score">
-                                <div id="score-display">20</div>
+                                <div id="score-display">{this.state.level}</div>
                                 <div className="controls-desc">score</div>
                             </div>
                             <div id="start">
-                                <div className="controls-led off"></div>
-                                <button className="controls-button"></button>
+                                <div className={(this.state.started ? 'on' : 'off') + ' controls-led'}></div>
+                                <button
+                                    className={(this.state.started ? 'pressed' : '') + ' controls-button'}
+                                    onClick={() => this.startGame()}
+                                ></button>
                                 <div className="controls-desc">start</div>
                             </div>
                             <div id="strict">
